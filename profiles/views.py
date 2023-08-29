7b5c002ddbcf5a29a9cfe83a0ca240199c7fe766
benjackson3811 <benjackson3811@googlemail.com>
2023-08-29 10:52:40 +0000

    adding profile filter and comment filter

--- profiles/views.py
+++ profiles/views.py
@@ -22,6 +22,7 @@ class ProfileList(generics.ListAPIView):
     ]
     filterset_fields = [
         'owner__following__followed__profile',
+        'owner__followed__owner__profile'
     ]
     OrderingFields =[
         'post_count',
